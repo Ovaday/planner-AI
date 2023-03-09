f1f2059281157657c4648b245811a90cfe708f7b
Ovaday <abdrazakowa@gmail.com>
2023-03-09 16:14:51 +0100

    ChatGPT bot fix system_content

--- helpers/openAIHelper.py
+++ helpers/openAIHelper.py
@@ -7,11 +7,13 @@ def chatGPT_req(message, tg_chat):
     if len(message) < 7:
         return None
     openai.api_key = get_token('OPENAI_API')
+    system_content = f"For the answer, use max limit of 200 words. Preferred language: {tg_chat.language}"
+    if tg_chat.role == 'admin':
+        system_content = f"For the answer, use max limit of 200 words"
     chatgpt_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"For the answer, use max limit of 200 words. "
-                                          f"Preferred language: {tg_chat.language}"},
+            {"role": "system", "content": system_content},
             {"role": "user", "content": message}
         ]
     )
