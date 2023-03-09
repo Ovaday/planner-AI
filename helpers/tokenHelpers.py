010855080ab96f767b513c6c5e50a3d3a8153ed6
Ovaday <abdrazakowa@gmail.com>
2023-03-09 13:08:11 +0100

    Environment variables fix

--- helpers/tokenHelpers.py
+++ helpers/tokenHelpers.py
@@ -1,12 +1,13 @@
 import os
+from decouple import config
 
 def get_token(token_name):
-    if not token_name:
+    if len(token_name) < 1:
         return None
 
     token_value = os.getenv(token_name)
     if token_value:
         return token_value
 
-    token_value = os.getenv('LOCAL_' + token_name)
+    token_value = config('LOCAL_' + str(token_name))
     return token_value
