dbe75b0e51bfcaef7ff183a4696b17c0eb81b88b
Ovaday <abdrazakowa@gmail.com>
2023-03-11 10:51:11 +0100

    Fix static files

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.template.defaulttags import url
-from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.views.decorators.csrf import csrf_exempt
 
 from example.views import TutorialBotView
@@ -25,3 +26,6 @@ urlpatterns = [
     path('', include('example.urls')),
     path('webhooks/tutorial/', csrf_exempt(TutorialBotView.as_view())),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
