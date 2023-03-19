fe93503c926d9ab265eebffbaee5a0066610f2c4
Evgenii <evgeny.abdrazakov@truesalessoft.com>
2023-03-20 00:07:36 +0100

    Fix static files

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -124,14 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT =os.path.join(PROJECT_ROOT,'static')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'vercel_app/static'),
-)
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
