40c4760766e0ff978d9574c5f1352e2471a826cc
Evgenii <abdrazakowa@gmail.com>
2023-03-27 18:14:02 +0300

    14 improve security (#15)
    * AWS secrets manager implementation
    
    Signed-off-by: Ovaday <abdrazakowa@gmail.com>
    
    * Fix formatting
    
    Signed-off-by: Ovaday <abdrazakowa@gmail.com>
    
    * Improve gitignore
    
    Signed-off-by: Ovaday <abdrazakowa@gmail.com>
    
    ---------
    
    Signed-off-by: Ovaday <abdrazakowa@gmail.com>

--- helpers/tokenHelpers.py
+++ helpers/tokenHelpers.py
@@ -1,5 +1,25 @@
 import os
 from decouple import config
+import boto3
+from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
+from django.conf import settings
+
+
+def retrieve_and_cache_secrets():
+    session = boto3.session.Session(
+        aws_access_key_id=get_token("IAM_ACCESS_KEY"),
+        aws_secret_access_key=get_token("IAM_SECRET_KEY"),
+    )
+
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=get_token("IAM_AWS_REGION")
+    )
+    cache_config = SecretCacheConfig()
+    cache = SecretCache(config=cache_config, client=client)
+
+    return cache
+
 
 def get_token(token_name):
     if len(token_name) < 1:
@@ -10,7 +30,21 @@ def get_token(token_name):
         return token_value
 
     try:
-        token_value = config(str(token_name))
+        if "IAM_" not in token_name or "DEBUG_MODE" not in token_name or config('ENV') == "PROD":
+            token_value = config(str(token_name))
+        else:
+            token_value = settings.SECRETS.get_secret_string("dev/plannerAI")[str(token_name)]
         return token_value
     except:
         return None
+
+
+def get_db_conn():
+    return {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_token("DB_NAME"),
+        'USER': get_token("DB_USER"),
+        'PASSWORD': get_token("DB_PASSWORD"),
+        'HOST': get_token("DB_HOST"),
+        'PORT': '5432',
+    }
