36c130175cafe3a664bda6567817f05dec44b988
Ovaday <abdrazakowa@gmail.com>
2023-04-08 16:55:52 +0200

    Fix vercel deployment rule
    Signed-off-by: Ovaday <abdrazakowa@gmail.com>

--- deployment_rule.sh
+++ deployment_rule.sh
@@ -5,12 +5,12 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 if [[ "$VERCEL_GIT_COMMIT_REF" == "mig" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
-  exit 0;
+  exit 1;
 elif [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
-  exit 0;
+  exit 1;
 else
   echo "🛑 - Build is intended to AWS and not for Vercel"
-  exit 1;
+  exit 0;
 fi
