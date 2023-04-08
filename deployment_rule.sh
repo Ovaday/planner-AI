1b34e9101d92b8c161599e525d96c98f41e5a2b6
Ovaday <abdrazakowa@gmail.com>
2023-04-08 15:00:46 +0200

    Fix deployment rule for AWS branch
    Signed-off-by: Ovaday <abdrazakowa@gmail.com>

--- deployment_rule.sh
+++ deployment_rule.sh
@@ -2,15 +2,15 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "mig-aws" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "mig" ]] ; then
   # Proceed with the build
-  echo "🛑 - Build is intended to AWS and not for Vercel"
+  echo "✅ - Build can proceed"
   exit 0;
-elif [[ "$VERCEL_GIT_COMMIT_REF" == "main-aws" ]] ; then
+elif [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
   # Proceed with the build
-  echo "🛑 - Build is intended to AWS and not for Vercel"
+  echo "✅ - Build can proceed"
   exit 0;
 else
-  echo "✅ - Build can proceed"
+  echo "🛑 - Build is intended to AWS and not for Vercel"
   exit 1;
 fi
