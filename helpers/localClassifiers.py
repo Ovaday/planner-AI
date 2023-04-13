caf114594708e8a0c731b0929db8c0f1e609e19e
Evgenii Abdrazakov <evgenii.abdrazakov@volytica.com>
2023-04-13 16:04:00 +0200

    Inserted newer models, local one is prioritized.

--- helpers/localClassifiers.py
+++ helpers/localClassifiers.py
@@ -4,11 +4,12 @@ from io import BytesIO
 import numpy as np
 import pandas as pd
 from helpers.resourcesPacker import read_encrypted
+from open_ai.helpers import fill_classification_template
 
 
 def load_classifier():
-    loaded_model_raw, f = read_encrypted('finalized_model.sav', is_normal_read=False)
-    loaded_vectorizer_raw, f = read_encrypted('vectorizer.sav', is_normal_read=False)
+    loaded_model_raw, f = read_encrypted('finalized_model_classification_95528.sav', is_normal_read=False)
+    loaded_vectorizer_raw, f = read_encrypted('vectorizer_BOTH_v2.sav', is_normal_read=False)
 
     loaded_model = pickle.load(BytesIO(loaded_model_raw))
     loaded_vectorizer = pickle.load(BytesIO(loaded_vectorizer_raw))
@@ -20,5 +21,5 @@ async def predict_class(message):
     model, vectorizer = load_classifier()
     mine_df = pd.DataFrame({'message': [str(message)]})
     mine_test = vectorizer.transform(mine_df['message'])
-    probabilities = np.array(model.predict(mine_test))
-    return probabilities[0]
+    classification_results = np.array(model.predict(mine_test))
+    return fill_classification_template(classification_results[0])
