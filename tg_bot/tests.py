3cea8d84d3512ccf8f902362d9be63b794b366ee
Kikrotobi <niki40275@gmail.com>
2023-04-20 02:54:40 +0400

    Add tests

--- tg_bot/tests.py
+++ tg_bot/tests.py
@@ -2,16 +2,30 @@ from django.test import TestCase
 from tg_bot.models import Chat
 
 
-# ToDo: Modify the test to execute correctly with the tg_bot.models - class Chat, instead of sample 'Animal'
-# run with python manage.py test
-class AnimalTestCase(TestCase):
+class ChatTestCase(TestCase):
     def setUp(self):
-        Animal.objects.create(name="lion", sound="roar")
-        Animal.objects.create(name="cat", sound="meow")
+        Chat.objects.create(chat_id="chat1", counter=0, tokens_used=0, is_approved=False,
+                            role='none', language='english', last_conversation='', username='',
+                            current_mode='chatGPT')
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        lion = Animal.objects.get(name="lion")
-        cat = Animal.objects.get(name="cat")
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
+        Chat.objects.create(chat_id="chat2", counter=0, tokens_used=0, is_approved=False,
+                            role='none', language='english', last_conversation='', username='',
+                            current_mode='chatGPT')
+
+    def test_money_used(self):
+        """Test money_used() method of Chat model"""
+        chat1 = Chat.objects.get(chat_id="chat1")
+        chat2 = Chat.objects.get(chat_id="chat2")
+        self.assertEqual(chat1.money_used(), '$ 0.0')  # Ожидаемое значение при counter=0, tokens_used=0
+        self.assertEqual(chat2.money_used(), '$ 0.0')  # Ожидаемое значение при counter=0, tokens_used=0
+
+        # Изменяем значения полей и проверяем результат
+        chat1.counter = 10
+        chat1.tokens_used = 100
+        chat1.save()
+        chat2.counter = 20
+        chat2.tokens_used = 200
+        chat2.save()
+
+        self.assertEqual(chat1.money_used(), '$ 0.002')  # Ожидаемое значение при counter=10, tokens_used=100
+        self.assertEqual(chat2.money_used(), '$ 0.004')
