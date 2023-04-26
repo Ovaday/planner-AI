3de41849ca2adb2bc8bed3aca55acf917c27eb16
anastasia <nastyakolbina2017@gmail.com>
2023-04-26 21:58:15 +0300

    test

--- example/views.py
+++ example/views.py
@@ -16,7 +16,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-        <h1>hell1</h1>
+        <h2>hell1</h2>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
         </body>
