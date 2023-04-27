53a1a155247a62eafc05e98f9cb30b72c73a1b5f
anastasia <nastyakolbina2017@gmail.com>
2023-04-27 23:57:19 +0300

    return blank list

--- website/views.py
+++ website/views.py
@@ -59,6 +59,8 @@ def get_messages(request, *args, **kwargs):
                 messages_list.append(message)
             json_string = json.dumps(messages_list,  ensure_ascii=False, indent=4,  sort_keys=True, default=str)
             return JsonResponse ({"messages": json_string})
+        else:
+            return messages_list
         # ToDo Anastasia: Retrieve here messages for the user with helpers.MessageHistoryHelpers.get_last_user_messages()
         # ToDo Anastasia: Include case when there are no messages.
 
