e025c90cb83f7b3dc1e2160436aa7dcce4272766
Kikrotobi <niki40275@gmail.com>
2023-04-28 14:50:15 +0400

    improve and add tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,10 +1,25 @@
+import os
 import unittest
+from pathlib import Path
 
 from helpers import MessageHistoryHelpers
 from helpers.openAIHelper import *
 from helpers.DatabaseHelpers import *
-from django.test import TestCase
 from telegram import Message
+from unittest.mock import AsyncMock
+from helpers.openAIHelper import chatGPT_req
+from helpers.resourcesPacker import listFiles, handleExtension, encrypt, decrypt, get_fernet_key, read_encrypted
+from asgiref.sync import sync_to_async
+import os
+from helpers.tokenHelpers import retrieve_and_cache_secrets, get_token, get_db_conn, get_mongo_db_conn
+from helpers.ScheduleHelpers import schedule_my_task, get_my_reminders, async_to_schedule
+import asyncio
+from unittest.mock import MagicMock, patch
+from django.test import TestCase
+from django_q.models import Schedule
+from service_routine.models import ServiceM
+from helpers.SQSHelpers import async_call_receiver, task_receiver
+
 
 # ToDO: add new tests
 class ParseResponseCase(TestCase):
@@ -218,3 +233,235 @@ class TestMessageHistoryHelpers(unittest.TestCase):
         result = self.message_history_helpers.get_last_user_messages(user_id)
         # Проверка типа результата
         self.assertIsInstance(result, list)
+
+class TestChatGPTReq(unittest.IsolatedAsyncioTestCase):
+    async def test_chatGPT_req(self):
+        message = "Hello, how are you?"
+        tg_chat = AsyncMock()
+        tg_chat.language = "en"
+        tg_chat.role = "user"
+        response_mock = AsyncMock()
+        response_mock.choices = [AsyncMock(message=AsyncMock(content="Hi there!"))]
+        response_mock.usage.total_tokens = 10
+        with patch("openai.ChatCompletion.create", return_value=response_mock) as chat_mock:
+            result = await chatGPT_req(message, tg_chat, "normal", "test-model")
+            chat_mock.assert_called_once_with(
+                model="test-model",
+                max_tokens=1000,
+                messages=[
+                    {"role": "system", "content": "Use max limit of 1000 words. Preferred language: en"},
+                    {"role": "user", "content": message}
+                ]
+            )
+        self.assertEqual(result, "Hi there!")
+
+root_file_path = Path(__file__).resolve().parent.parent
+basedir_extracted = os.path.join(root_file_path, 'resources/extracted/')
+basedir_packed = os.path.join(root_file_path, 'resources/')
+
+def test_listFiles():
+    # Test for when is_encrypt is True
+    assert listFiles(basedir_packed, True) == ['file1.enc', 'file2.enc']
+
+    # Test for when is_encrypt is False
+    assert listFiles(basedir_packed, False) == ['file1.txt', 'file2.jpg']
+
+def test_handleExtension():
+    # Test for when is_encrypt is True
+    assert handleExtension('file1.txt', True) == 'file1.txt.enc'
+    assert handleExtension('file1.txt.enc', True) == 'file1.txt.enc'
+
+    # Test for when is_encrypt is False
+    assert handleExtension('file1.txt.enc', False) == 'file1.txt'
+    assert handleExtension('file1.txt', False) == 'file1.txt'
+
+def test_encrypt_decrypt():
+    key = get_fernet_key()
+
+    # Test encryption and decryption for a text file
+    filename_origin = basedir_extracted + 'file1.txt'
+    filename_destination = basedir_packed + 'file1.txt.enc'
+    encrypt(filename_origin, filename_destination, key)
+    decrypted_data = read_encrypted('file1.txt', key, is_normal_read=False)
+    assert decrypted_data == b'test'
+
+    filename_origin = basedir_packed + 'file1.txt.enc'
+    filename_destination = basedir_extracted + 'file1.txt'
+    decrypt(filename_origin, filename_destination, key)
+    with open(filename_destination, "r") as file:
+        assert file.read() == 'test'
+
+    # Test encryption and decryption for a binary file
+    filename_origin = basedir_extracted + 'file2.jpg'
+    filename_destination = basedir_packed + 'file2.jpg.enc'
+    encrypt(filename_origin, filename_destination, key)
+    decrypted_data = read_encrypted('file2.jpg', key, is_normal_read=False)
+    with open(filename_origin, "rb") as file:
+        assert decrypted_data == file.read()
+
+    filename_origin = basedir_packed + 'file2.jpg.enc'
+    filename_destination = basedir_extracted + 'file2.jpg'
+    decrypt(filename_origin, filename_destination, key)
+    with open(filename_destination, "rb") as file:
+        assert file.read() == decrypted_data
+
+def test_read_encrypted():
+    key = get_fernet_key()
+
+    # Test reading an encrypted text file
+    filename = 'file1.txt.enc'
+    decrypted_data = read_encrypted(filename, key)
+    assert decrypted_data.getvalue() == 'test\n'
+
+    # Test reading an encrypted binary file
+    filename = 'file2.jpg.enc'
+    decrypted_data = read_encrypted(filename, key, is_normal_read=False)
+    with open(basedir_extracted + 'file2.jpg', "rb") as file:
+        assert decrypted_data == file.read()
+
+
+class TestScheduleHelpers(TestCase):
+
+    def setUp(self):
+        self.chat_id = 123
+        self.future_time = '2023-05-05 18:00:00'
+        self.request_data = {'text': 'Test request'}
+
+    def test_schedule_my_task(self):
+        scheduled_obj = schedule_my_task(self.future_time)
+        self.assertIsInstance(scheduled_obj, Schedule)
+        self.assertEqual(scheduled_obj.func, 'tg_routine.main.task_handler')
+        self.assertEqual(scheduled_obj.schedule_type, 'O')
+        self.assertEqual(scheduled_obj.next_run, self.future_time)
+        self.assertEqual(scheduled_obj.repeats, 1)
+
+    @sync_to_async
+    def create_service_m_obj(self):
+        return ServiceM.objects.create(chat_id=self.chat_id)
+
+    def test_get_my_reminders(self):
+        # create mock schedule object with success method
+        mock_schedule_obj = MagicMock()
+        mock_schedule_obj.success.return_value = False
+
+        # create service_m object with schedule object
+        service_m_obj = self.create_service_m_obj()
+        service_m_obj.schedule = mock_schedule_obj
+        service_m_obj.request = self.request_data
+        service_m_obj.save()
+
+        # get reminders and assert that service_m_obj is returned
+        result = get_my_reminders(service_m_obj.chat)
+        expected_result = {
+            service_m_obj.id: {
+                'request': self.request_data,
+                'schedule': mock_schedule_obj
+            }
+        }
+        self.assertEqual(result, expected_result)
+
+    def test_async_to_schedule(self):
+        chat = MagicMock()
+        chat.id = self.chat_id
+
+        scheduled_obj = MagicMock()
+
+        with self.assertLogs() as cm:
+            async_to_schedule(chat, self.future_time, self.request_data)
+
+        service_m_obj = ServiceM.objects.get(chat_id=self.chat_id)
+        self.assertEqual(service_m_obj.chat, chat)
+        self.assertEqual(service_m_obj.request, self.request_data)
+        self.assertEqual(service_m_obj.schedule, scheduled_obj)
+        self.assertRegex(str(cm.output), 'Try to exec!.*Woke up after execution!')
+
+
+class TestSQSHelpers(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Создаем фиктивный объект Schedule и ServiceM для тестов
+        cls.schedule_obj = Schedule.objects.create(func='tg_routine.main.task_handler',
+                                                   schedule_type='O',
+                                                   next_run=asyncio.get_event_loop().time() + 5,
+                                                   repeats=1)
+        cls.chat_id = 1234
+        cls.service_m_obj = ServiceM.objects.create(chat_id=cls.chat_id, schedule=cls.schedule_obj, request='test request')
+
+    @patch('tg_routine.main.task_handler')
+    def test_task_receiver(self, mock_task_handler):
+        update_json = {'test': 'update_json'}
+        task_receiver(update_json)
+        mock_task_handler.assert_called_once_with(update_json)
+
+    @patch('helpers.openAIHelper.chatGPT_req_test')
+    def test_async_call_receiver(self, mock_chatGPT_req_test):
+        message = 'test message'
+        mock_chatGPT_req_test.return_value = 'test response'
+        result = async_call_receiver(message)
+        mock_chatGPT_req_test.assert_called_once_with(message, None, None, None, None)
+        self.assertEqual(result, 'test response')
+
+class TestTokenHelpers:
+    def test_retrieve_and_cache_secrets(self):
+        with patch.dict(os.environ, {
+            'IAM_ACCESS_KEY': 'access_key',
+            'IAM_SECRET_KEY': 'secret_key',
+            'IAM_AWS_REGION': 'us-west-2'
+        }):
+            cache = retrieve_and_cache_secrets()
+            assert cache.get_secret_string('my_secret') == 'my_secret_value'
+
+    def test_get_token_with_env_variable(self):
+        with patch.dict(os.environ, {'MY_TOKEN': 'my_token_value'}):
+            assert get_token('MY_TOKEN') == 'my_token_value'
+
+    def test_get_token_with_vercel(self):
+        with patch.dict(os.environ, {'VERCEL': 'true'}), \
+             patch('tokenHelpers.settings', spec_set=['SECRETS']) as mock_settings, \
+             patch('tokenHelpers.json.loads', return_value={'MY_TOKEN': 'my_token_value'}):
+            mock_settings.SECRETS.get_secret_string.return_value = '{"MY_TOKEN": "my_token_value"}'
+            assert get_token('MY_TOKEN') == 'my_token_value'
+
+    def test_get_token_with_iam(self):
+        with patch('tokenHelpers.config', return_value='my_iam_token_value'):
+            assert get_token('IAM_TOKEN') == 'my_iam_token_value'
+
+    def test_get_token_with_secrets_path(self):
+        with patch('tokenHelpers.config', return_value='secrets_path'), \
+             patch('tokenHelpers.settings', spec_set=['SECRETS']), \
+             patch('tokenHelpers.json.loads', return_value={'MY_TOKEN': 'my_token_value'}):
+            mock_secrets = type('Secrets', (), {'get_secret_string': lambda self, path: '{"MY_TOKEN": "my_token_value"}'})
+            mock_settings = type('Settings', (), {'SECRETS': mock_secrets})
+            assert get_token('MY_TOKEN') == 'my_token_value'
+
+    def test_get_db_conn(self):
+        with patch.dict(os.environ, {
+            'DB_NAME': 'my_db_name',
+            'DB_USER': 'my_db_user',
+            'DB_PASSWORD': 'my_db_password',
+            'DB_HOST': 'my_db_host'
+        }):
+            expected_conn = {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'my_db_name',
+                'USER': 'my_db_user',
+                'PASSWORD': 'my_db_password',
+                'HOST': 'my_db_host',
+                'PORT': '5432',
+            }
+            assert get_db_conn() == expected_conn
+
+    def test_get_mongo_db_conn(self):
+        with patch.dict(os.environ, {
+            'MONGO_DB_USER': 'my_mongo_user',
+            'MONGO_DB_PASSWORD': 'my_mongo_password',
+            'MONGO_DB_ENVIROMENT': 'my_mongo_host'
+        }):
+            expected_conn = {
+                'USER': 'my_mongo_user',
+                'PASSWORD': 'my_mongo_password',
+                'HOST': 'my_mongo_host',
+            }
+            assert get_mongo_db_conn() == expected_conn
+
